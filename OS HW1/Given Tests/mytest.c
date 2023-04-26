174db72fc45827817deb935b43b4a4b574c68731
hassankhalil1996 <hassan21kh1996@gmail.com>
2023-04-26 21:54:11 +0300

    update mysecret test
    fix strcpy

--- OS HW1/Given Tests/mytest.c
+++ OS HW1/Given Tests/mytest.c
@@ -4,12 +4,14 @@
 #include <sys/types.h>
 #include "magic_api.h"
 #define NUM_CHILDREN 3
+#define NUM_SECRET 2
 
 int main() {
     pid_t pids[NUM_CHILDREN];
     int status;
     int i =0 ;
     int r ;
+    char secrets[NUM_SECRET][SECRET_MAXSIZE];
 
     // create child processes
     for ( i = 0; i < NUM_CHILDREN; i++) {
@@ -17,15 +19,34 @@ int main() {
         if (pids[i] == 0) {
             // child process
             printf("Child process %d started (PID: %d)\n", i + 1, getpid());
-	    
-	    r =magic_get_wand(10, "1234567891234567891234567891111111111");
-	    printf("first get faile r =%d \n",r);
-	    r =magic_get_wand(10, "hii");
-	    printf("seconde get success r= %d \n",r);
-	    printf("r =%d(success)\n",r);
-	    printf("child %d attacking process %d \n",i,pids[0]);
-	    //int health = magic_attack(pids[0]);
-	    printf("target pid = %d\n",pids[0]);
+	    switch(i){
+	    case 0:
+	    	r =magic_get_wand(10, "secret0");
+		break;
+	    case 1:
+	    	r =magic_get_wand(10, "secret1");
+		break;
+	    case 2:
+	    	r =magic_get_wand(10, "secret2");
+		break;
+	    }
+
+	    printf("r = %d(success)\n",r);
+	    printf("child %d stealing from process %d \n",i,pids[0]);
+	    r = magic_legilimens(pids[0]);
+	    printf("r = %d(success)\n",r);
+
+	    r = magic_list_secrets(secrets, NUM_SECRET);
+	    printf("r = %d\n",r);
+	
+	    for(i=0;i<NUM_SECRET;i++){
+		printf("secret number %d : %s \n",i,secrets[i]);
+	    }
+
+            //r = magic_legilimens(pids[0]);
+	    //printf("r = %d(success)\n",r);
+	    //int health = magic_attack(c);
+	    //printf("target pid = %d\n",pids[0]);
 	    //printf("health = %d\n",health);
             sleep(2);
             printf("Child process %d finished (PID: %d)\n", i + 1, getpid());
