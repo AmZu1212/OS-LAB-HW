6c8eec0f1d6c4eec4ed90e28ba33143475ded2c1
AmZu12 <amirzuabi@campus.technion.ac.il>
2023-06-13 19:35:59 +0300

    finished major repeated.c functions

--- OS HW3/hw3_helper_files/repeated.c
+++ OS HW3/hw3_helper_files/repeated.c
@@ -25,6 +25,7 @@ MODULE_LICENSE("GPL");
 int my_major = 0; /* will hold the major # of my device driver */
 char *module_string = NULL;
 int max_size = 0;
+int string_size = 0;
 
 
 struct file_operations my_fops = {
@@ -39,18 +40,14 @@ struct file_operations my_fops = {
 int init_module(void)
 {
     // This function is called when inserting the module using insmod
-
     my_major = register_chrdev(my_major, MY_DEVICE, &my_fops);
-
+    module_string = NULL;
     if (my_major < 0)
     {
-	printk(KERN_WARNING "can't get dynamic major\n");
-	return my_major;
+        printk(KERN_WARNING "can't get dynamic major\n");
+        return my_major;
     }
 
-    //
-    // do_init();
-    //
     return 0;
 }
 
@@ -60,18 +57,21 @@ void cleanup_module(void)
     // This function is called when removing the module using rmmod
 
     unregister_chrdev(my_major, MY_DEVICE);
+    if (module_string != NULL) {
+        kfree(module_string);
+        module_string = NULL;
+    }
 
-    //
-    // do clean_up();
-    //
     return;
 }
 
-/* initialize values needed for operation if they were not already, default string should be NULL, and max size = 0*/
+/* Initialize values needed for operation if they were not already, default string should be NULL, and max size = 0*/
 int my_open(struct inode *inode, struct file *filp)
 {
     // handle open
-
+    //unsigned int minor = MINOR(inode->i_rdev);
+    unsigned int minor = iminor(inode);
+    filp->f_pos = 0;
     return 0;
 }
 
@@ -79,25 +79,60 @@ int my_open(struct inode *inode, struct file *filp)
 int my_release(struct inode *inode, struct file *filp)
 {
     // handle file closing
-
+    // maybe not needed?
     return 0;
 }
 
+// Does the read operation. Returns the amount of bytes added to the buffer
 ssize_t my_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
-    //
-    // Do read operation.
-    // Return the amount of bytes added to the buffer
+    // 1 char = 1 byte = 8 bits
+    if (buf == NULL) {
+        return -EFAULT;
+    }
 
-    return 0; 
+    //bool full = false;
+    int bytes_read = 0;
+    int allowed_bytes = max_size - *f_pos;
+
+    int leftover = *f_pos
+    while(leftover >= string_size) {
+        leftover -=string_size;
+    }
+
+    int string_pos = leftover;
+    for(;bytes_read < allowed_bytes; bytes_read++)
+    {
+        if(bytes_read == count - 1) { 
+            //full = true
+            break;
+        }
+        
+        //copy byte to user
+        if (copy_to_user(&buf[bytes_read], &module_string[string_pos], 1) != 0) {
+            return -EBADF;
+        }
+
+        string_pos++;
+
+        if(string_pos >= string_size)
+        {
+            string_pos = 0;
+        }
+    }
+    
+    *f_pos += bytes_read;
+
+    return ++bytes_read;
 }
 
+// Ignores writing, but updates max_size to max_size+count, returns count
 ssize_t my_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
     size_t new_size = max_size + count;
-    char *newstring = kmalloc((new_size + 1) * sizeof(char), GFP_KERNEL);
 
-    //maybe check for malloc error
+    /* char *newstring = kmalloc((new_size + 1) * sizeof(char), GFP_KERNEL);
+
     if(newstring == NULL) {
         //kmalloc() error
         return -ENOMEM;
@@ -109,18 +144,19 @@ ssize_t my_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
         char temp = module_string[i];
         newstring[i] = temp;
         i++;
-    }
+    } 
 
+    memcpy(newstring, module_string, max_size);
     newstring[new_size] = '\0';
 
-    free(module_string);
+    kfree(module_string);
+    module_string = newstring; */
 
-    module_string = newstring;
     max_size = new_size;
-
     return count; 
 }
 
+// Sets llseek to be buffer_pointer+shift. if it over flows you reach 0 or max_size
 loff_t my_llseek(struct file *flip, loff_t buffer_pointer, int shift) {
     
     // 0 <-> max_size
@@ -138,23 +174,56 @@ loff_t my_llseek(struct file *flip, loff_t buffer_pointer, int shift) {
     return newpos;
 }
 
-
+// Our IOCTL procedures.
 int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg)
 {
     switch(cmd)
     {
-    case SET_STRING:// fills the string
-	//
-	// handle 
-	//
-	break;
+    case SET_STRING: // fills the string
+
+        // UPDATE STRING SIZE
+        // kmalloc is (string_size + 1)
+        // also dont forget to do \0 in the end of the string
+
+        char* new_string = (char*)arg;
+        if(module_string !=NULL){
+            kfree(module_string);
+            module_string = NULL;
+        }
+
+        int i = 0;
+        // is the new string terminated by '\0'?
+        while(new_string[i] != '\0') { 
+            i++;
+        } 
+        string_size = i;
+        module_string = kmalloc(sizeof(char)*(string_size + 1), GFP_KERNEL);
+        if (module_string == NULL) {
+            // kmalloc() error
+            return -ENOMEM; 
+        }
+
+        if (copy_from_user(module_string, new_string, string_size) != 0) {
+            // Error copying string from user
+            kfree(module_string);
+            module_string = NULL;
+            return -EBADF; 
+        }
+        module_string[i] = '\0';
+        
+	    break;
+
     case RESET: // returns the buffer to string = null
-	//
-	// handle 
-	//
-	break;
+        max_size = 0;
+        string_size = 0;
+        if(module_string !=NULL) {
+            kfree(module_string);
+            module_string = NULL;
+        }
+	    break;
+    
     default:
-	return -ENOTTY;
+	    return -ENOTTY;
     }
 
     return 0;
--- OS HW3/hw3_helper_files/repeated.h
+++ OS HW3/hw3_helper_files/repeated.h
@@ -4,17 +4,16 @@
 #include <linux/ioctl.h>
 #include <linux/types.h>
 
-//
-// Function prototypes
-//
+
+// Module Functions
 int my_open(struct inode *, struct file *);
-
 int my_release(struct inode *, struct file *);
-
-ssize_t my_read(struct file *, char *, size_t, loff_t *);
-
 int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg);
+ssize_t my_write(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+ssize_t my_read(struct file *, char *, size_t, loff_t *);
+loff_t my_llseek(struct file *flip, loff_t buffer_pointer, int shift);
 
+// Module Defines
 #define MY_MAGIC 'r'
 #define SET_STRING  _IOW(MY_MAGIC, 0, char*)
 #define RESET  _IO(MY_MAGIC, 1)
