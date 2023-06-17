1a637bd965d2cbfc52059e64568cd9215718890a
AmZu12 <amirzuabi@campus.technion.ac.il>
2023-06-17 18:41:01 +0300

    Update repeated.c

--- OS HW3/repeated.c
+++ OS HW3/repeated.c
@@ -6,11 +6,11 @@
 #define LINUX
 #define __KERNEL__
 
-#include <linux/kernel.h>  	
+#include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/fs.h>       		
+#include <linux/fs.h>
 #include <asm/uaccess.h>
-#include <linux/errno.h>  
+#include <linux/errno.h>
 #include <asm/segment.h>
 #include <asm/current.h>
 
@@ -26,7 +26,6 @@ int my_major = 0; /* will hold the major # of my device driver */
 char *module_string = NULL;
 int max_size = 0;
 int string_size = 0;
-
 
 struct file_operations my_fops = {
     .open = my_open,
@@ -51,13 +50,13 @@ int init_module(void)
     return 0;
 }
 
-
 void cleanup_module(void)
 {
     // This function is called when removing the module using rmmod
 
     unregister_chrdev(my_major, MY_DEVICE);
-    if (module_string != NULL) {
+    if (module_string != NULL)
+    {
         kfree(module_string);
         module_string = NULL;
     }
@@ -69,12 +68,12 @@ void cleanup_module(void)
 int my_open(struct inode *inode, struct file *filp)
 {
     // handle open
-    //unsigned int minor = MINOR(inode->i_rdev);
-    unsigned int minor = iminor(inode);
+    // unsigned int minor = MINOR(inode->i_rdev);
+    unsigned int minor = iminor(inode); // IS THIS USELESS?
+    access_ok();
     filp->f_pos = 0;
     return 0;
 }
-
 
 int my_release(struct inode *inode, struct file *filp)
 {
@@ -87,40 +86,49 @@ int my_release(struct inode *inode, struct file *filp)
 ssize_t my_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
     // 1 char = 1 byte = 8 bits
-    if (buf == NULL) {
+    if (buf == NULL)
+    {
         return -EFAULT;
     }
 
-    //bool full = false;
+    if (access_ok(VERIFY_READ, (void __user *)buf, count) == FALSE)
+    {
+        return -EBADF;
+    }
+
+    // bool full = false;
     int bytes_read = 0;
     int allowed_bytes = max_size - *f_pos;
 
-    int leftover = *f_pos
-    while(leftover >= string_size) {
-        leftover -=string_size;
+    int leftover = *f_pos;
+    while (leftover >= string_size)
+    {
+        leftover -= string_size;
     }
 
     int string_pos = leftover;
-    for(;bytes_read < allowed_bytes; bytes_read++)
+    for (; bytes_read < allowed_bytes; bytes_read++)
     {
-        if(bytes_read == count - 1) { 
-            //full = true
+        if (bytes_read == count - 1)
+        {
+            // full = true
             break;
         }
-        
-        //copy byte to user
-        if (copy_to_user(&buf[bytes_read], &module_string[string_pos], 1) != 0) {
+
+        // copy byte to user
+        if (copy_to_user(&buf[bytes_read], &module_string[string_pos], 1) != 0)
+        {
             return -EBADF;
         }
 
         string_pos++;
 
-        if(string_pos >= string_size)
+        if (string_pos >= string_size)
         {
             string_pos = 0;
         }
     }
-    
+
     *f_pos += bytes_read;
 
     return ++bytes_read;
@@ -130,43 +138,24 @@ ssize_t my_read(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 ssize_t my_write(struct file *filp, char *buf, size_t count, loff_t *f_pos)
 {
     size_t new_size = max_size + count;
-
-    /* char *newstring = kmalloc((new_size + 1) * sizeof(char), GFP_KERNEL);
-
-    if(newstring == NULL) {
-        //kmalloc() error
-        return -ENOMEM;
-    }
-    
-    int i = 0;
-    while(module_string[i] != NULL) {
-
-        char temp = module_string[i];
-        newstring[i] = temp;
-        i++;
-    } 
-
-    memcpy(newstring, module_string, max_size);
-    newstring[new_size] = '\0';
-
-    kfree(module_string);
-    module_string = newstring; */
-
     max_size = new_size;
-    return count; 
+    return count;
 }
 
 // Sets llseek to be buffer_pointer+shift. if it over flows you reach 0 or max_size
-loff_t my_llseek(struct file *flip, loff_t buffer_pointer, int shift) {
-    
+loff_t my_llseek(struct file *flip, loff_t buffer_pointer, int shift)
+{
+
     // 0 <-> max_size
     size_t newpos = buffer_pointer + shift;
 
-    if(newpos >= max_size) {
+    if (newpos >= max_size)
+    {
         newpos = max_size;
     }
 
-    if(newpos <= 0) {
+    if (newpos <= 0)
+    {
         newpos = 0;
     }
 
@@ -177,7 +166,7 @@ loff_t my_llseek(struct file *flip, loff_t buffer_pointer, int shift) {
 // Our IOCTL procedures.
 int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg)
 {
-    switch(cmd)
+    switch (cmd)
     {
     case SET_STRING: // fills the string
 
@@ -185,45 +174,50 @@ int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned 
         // kmalloc is (string_size + 1)
         // also dont forget to do \0 in the end of the string
 
-        char* new_string = (char*)arg;
-        if(module_string !=NULL){
+        char *new_string = (char *)arg;
+        if (module_string != NULL)
+        {
             kfree(module_string);
             module_string = NULL;
         }
 
         int i = 0;
         // is the new string terminated by '\0'?
-        while(new_string[i] != '\0') { 
+        while (new_string[i] != '\0')
+        {
             i++;
-        } 
+        }
         string_size = i;
-        module_string = kmalloc(sizeof(char)*(string_size + 1), GFP_KERNEL);
-        if (module_string == NULL) {
+        module_string = kmalloc(sizeof(char) * (string_size + 1), GFP_KERNEL);
+        if (module_string == NULL)
+        {
             // kmalloc() error
-            return -ENOMEM; 
+            return -ENOMEM;
         }
 
-        if (copy_from_user(module_string, new_string, string_size) != 0) {
+        if (copy_from_user(module_string, new_string, string_size) != 0)
+        {
             // Error copying string from user
             kfree(module_string);
             module_string = NULL;
-            return -EBADF; 
+            return -EBADF;
         }
         module_string[i] = '\0';
-        
-	    break;
+
+        break;
 
     case RESET: // returns the buffer to string = null
         max_size = 0;
         string_size = 0;
-        if(module_string !=NULL) {
+        if (module_string != NULL)
+        {
             kfree(module_string);
             module_string = NULL;
         }
-	    break;
-    
+        break;
+
     default:
-	    return -ENOTTY;
+        return -ENOTTY;
     }
 
     return 0;
